4d395c4cda4b12606a976c195dfb2d82cb23e6b0
ntwari-egide <ntwariegide2@gmail.com>
2022-05-30 15:12:45 +0200

    ft(routing): implement routing in products

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import helmet from 'helmet'
 import * as dotenv from  'dotenv'
 import mongoose from 'mongoose'
 import express from 'express'
+import * as productController from  './controllers/products'
 
 dotenv.config();
 
@@ -23,3 +24,13 @@ app.use(express.json())
 const server = app.listen(PORT, () => console.log(`[SERVER] listening on a port ${PORT}`));
 
 app.get('/', (req,res) => res.send('Welcome to Node js with typescript template'))
+
+
+/**
+ * routing the requests
+ */
+
+app.get('/products', productController.getAllProductsList)
+app.post('/products', productController.createProduct)
+app.put('/products', productController.updateProduct)
+app.delete('/products', productController.deleteProduct)
